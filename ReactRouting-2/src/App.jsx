f574f349dec74a6c11a7d6f9db98bc3e8e4c32af
Renuka-285 <boskareunka@gmail>
2025-12-25 22:42:11 +0530

    protected route

---
+++ ReactRouting-2/src/App.jsx
@@ -0,0 +1,38 @@
+
+import { Routes, Route } from "react-router-dom";
+import Home from "./Home";
+import Login from "./Login";
+import Todos from "./Todos";
+import TodoDetails from "./TodoDetails";
+import ProtectedRoute from "./ProtectedRoute";
+
+function App() {
+  return (
+    <Routes>
+      {/* Unprotected Routes */}
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+
+      {/* Protected Routes */}
+      <Route
+        path="/todos"
+        element={
+          <ProtectedRoute>
+            <Todos />
+          </ProtectedRoute>
+        }
+      />
+
+      <Route
+        path="/todos/:todoId"
+        element={
+          <ProtectedRoute>
+            <TodoDetails />
+          </ProtectedRoute>
+        }
+      />
+    </Routes>
+  );
+}
+
+export default App;
